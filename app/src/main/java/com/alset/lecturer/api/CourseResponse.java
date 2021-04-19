29f58bfb1979a4e97dc2d80ab54f30d2755e0f19
Chrishan Jayakody MS20922184 <ms20922184@my.sliit.lk>
2021-04-19 16:39:29 +0530

    Update course API response

--- app/src/main/java/com/alset/lecturer/api/CourseResponse.java
+++ app/src/main/java/com/alset/lecturer/api/CourseResponse.java
@@ -1,4 +1,20 @@
 package com.alset.lecturer.api;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CourseResponse {
+
+    @SerializedName("course_id")
+    private String courseId;
+
+    @SerializedName("course_name")
+    private String courseName;
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
 }
