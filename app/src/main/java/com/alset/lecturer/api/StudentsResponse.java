e6ff63318941ff3674511a346a40f2abc8d28b15
Chrishan Jayakody MS20922184 <ms20922184@my.sliit.lk>
2021-04-27 07:42:49 +0530

    Mapped Students response

--- app/src/main/java/com/alset/lecturer/api/StudentsResponse.java
+++ app/src/main/java/com/alset/lecturer/api/StudentsResponse.java
@@ -1,4 +1,69 @@
 package com.alset.lecturer.api;
 
+import com.google.gson.annotations.SerializedName;
+
 public class StudentsResponse {
+
+    @SerializedName("student_id")
+    private String studentId;
+
+    @SerializedName("f_name")
+    private String firstName;
+
+    @SerializedName("l_name")
+    private String lastName;
+
+    @SerializedName("phone")
+    private String phone;
+
+    @SerializedName("district")
+    private String district;
+
+    @SerializedName("email")
+    private String email;
+
+    @SerializedName("source_image_key")
+    private String sourceImageKey;
+
+    @SerializedName("dob")
+    private String dob;
+
+    @SerializedName("course_id")
+    private String courseId;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSourceImageKey() {
+        return sourceImageKey;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
 }
