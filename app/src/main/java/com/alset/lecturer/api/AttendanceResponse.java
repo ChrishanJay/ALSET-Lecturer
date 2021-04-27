c1233f91e3498bc465f394bd8f3bd113f8b2316a
Chrishan Jayakody MS20922184 <ms20922184@my.sliit.lk>
2021-04-27 07:42:20 +0530

    Mapped Attendance Response

--- app/src/main/java/com/alset/lecturer/api/AttendanceResponse.java
+++ app/src/main/java/com/alset/lecturer/api/AttendanceResponse.java
@@ -1,4 +1,41 @@
 package com.alset.lecturer.api;
 
+import com.google.gson.annotations.SerializedName;
+
 public class AttendanceResponse {
+
+    @SerializedName("student_id")
+    private String studentId;
+
+    @SerializedName("attended_time")
+    private String attendedTime;
+
+    @SerializedName("attended")
+    private int attended;
+
+    @SerializedName("attendance_id")
+    private String attendanceId;
+
+    @SerializedName("class_id")
+    private String classId;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getAttendedTime() {
+        return attendedTime;
+    }
+
+    public int getAttended() {
+        return attended;
+    }
+
+    public String getAttendanceId() {
+        return attendanceId;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
 }
